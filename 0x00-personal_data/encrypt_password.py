26fe24ad64a39380ee9747e182330cc885f21d04
Bonaventure Anselm <bona.anselm@gmail.com>
2023-08-10 07:58:27 +0100

    Task 5

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ Returns a salted hashed password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Checks if a hashed password is the same as the gievn password """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
