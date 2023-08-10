c7fd0b6009e9ac1bf95be74b402c8200352b3e22
Bonaventure Anselm <bona.anselm@gmail.com>
2023-08-10 07:45:31 +0100

    Task 4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,12 +21,12 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         """ Ffilter values in incoming log records using filter_datum """
-        record_info = super(RedactingFormatter, self).format(record)
-        txts = filter_datum(self.fields,
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields,
                             self.REDACTION,
-                            record_info,
+                            msg,
                             self.SEPARATOR)
-        return txts
+        return txt
 
 
 patterns = {
@@ -64,7 +64,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "Ej10f0r!")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     connection = mysql.connector.connect(
         host=db_host,
         port=3306,
@@ -73,6 +73,28 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         database=db_name,
     )
     return connection
+
+
+def main():
+    """ Logs the information about user records in a table """
+    fields = "name, email, phone, ssn,\
+              password, ip, last_login, user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
 
 
 if __name__ == "__main__":
