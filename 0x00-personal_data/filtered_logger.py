36a196a93b61127a9372f6fee41ad88e84e3820e
Bonaventure Anselm <bona.anselm@gmail.com>
2023-08-10 07:12:16 +0100

    Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,26 @@
 """ Defines filter_datum function """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Ffilter values in incoming log records using filter_datum """
+        message = super(RedactingFormatter, self).format(record)
+        texts = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+        return texts
 
 
 patterns = {
@@ -19,3 +39,7 @@ def filter_datum(
     """ Filters a log messages using regex """
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+if __name__ == "__main__":
+    main()
