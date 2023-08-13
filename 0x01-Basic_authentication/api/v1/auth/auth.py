bf1f1922f8d3c4fdc6cdb1c4d14d18c43e04cf80
Bonaventure Anselm <bona.anselm@gmail.com>
2023-08-13 05:46:39 +0100

    Task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Defines the class Auth """
+from flask import request
+
+
+class Auth:
+    """ Manages authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Checks that authorization is required """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Retrieves the authorization header """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Gets the current user """
+        return None
