1e001fa0f6b118adf45b18738b5c676c8238dbe1
Bonaventure Anselm <bona.anselm@gmail.com>
2023-08-14 06:35:24 +0100

    Task 5 to 11

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -25,6 +25,8 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Retrieves the authorization header """
+        if request:
+            return request.headers.get("Authorization", None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
