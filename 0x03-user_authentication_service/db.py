59dc2e59ce0c9c1f7e1dace5bf08ff8196c1b3fd
Bonaventure Anselm <bona.anselm@gmail.com>
2023-08-17 21:22:58 +0100

    Task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,8 +44,7 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """ Retrieves a user based on a set of filters """
-        fields = []
-        values = []
+        fields, values = [], []
         for key, value in kwargs.items():
             if hasattr(User, key):
                 fields.append(getattr(User, key))
@@ -58,3 +57,20 @@ class DB:
         if result is None:
             raise NoResultFound()
         return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ Finds and updates a user based on a given id """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_source[getattr(User, key)] = value
+            else:
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
+        self._session.commit()
